63709c741aa819aa4f6f4d0a280f09d45df2991f
Laytan Laats <laytanlaats@hotmail.com>
2023-12-27 01:25:46 +0100

    fix odin-doc path

--- docs/generate.sh
+++ docs/generate.sh
@@ -12,13 +12,13 @@ odin doc .. -all-packages -doc-format
 cd build
 
 # This is the binary of https://github.com/laytan/pkg.odin-lang.org, built by `odin built . -out:odin-doc`
-odin-doc ../docs.odin-doc ../odin-doc.json
+odin-doc ../odin-tree-sitter.odin-doc ../odin-doc.json
 
 # For GitHub pages, a CNAME file with the intended domain is required.
 echo "odin-tree-sitter.laytan.dev" > CNAME
 
 cd ..
 
-rm docs.odin-doc
+rm odin-tree-sitter.odin-doc
 
 cd ..
